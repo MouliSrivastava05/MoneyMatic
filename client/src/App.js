49d611537e376e60b49a843b80b783d15daf8402
Mouli Srivastava <moulisrivastava@Moulis-MacBook-Pro.local>
2025-09-14 20:17:36 +0530

    Intial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* Add dashboard, etc. later */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
